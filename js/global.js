bfa29b2c18d9cb6780d676cf4c30666616b3a6b2
Matthew <matthew.martijn.wilson@gmail.com>
2018-11-27 15:18:01 +0100

    Added slider

--- js/global.js
+++ js/global.js
@@ -1,5 +1,29 @@
 (function() {
    // our page initialization code here
    // the DOM will be available here
-
+   init();
 })();
+
+function init() {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
+}
