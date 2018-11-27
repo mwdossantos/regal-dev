7cad68c02efd68d709bb43ac1b6c6e8ba24f1c59
Matthew <matthew.martijn.wilson@gmail.com>
2018-11-28 00:38:31 +0100

    just store is left and cms

--- js/global.js
+++ js/global.js
@@ -33,6 +33,17 @@ function listener() {
   $('.yt').click(function() {
     navigator('https://www.youtube.com/user/teamAeRa','_blank');
   })
+
+  $('.aporia').click(function() {
+    navigator('https://aporiacustoms.com/','_blank');
+  })
+  $('.aukey').click(function() {
+    navigator('https://www.aukey.com/','_blank');
+  })
+  $('.perform').click(function() {
+    navigator('https://www.beaperformer.com/','_blank');
+  })
+
 }
 
 function navigator(destination, where) {
